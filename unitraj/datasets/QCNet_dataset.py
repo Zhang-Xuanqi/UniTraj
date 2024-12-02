5008de0b6f8fd7aca9f9bbcfbff7baed53719c1b
Zhang-Xuanqi <xuanqiz0026@gmail.com>
2024-12-02 14:04:12 +0100

    fix pt2pl edge_index

--- unitraj/datasets/QCNet_dataset.py
+++ unitraj/datasets/QCNet_dataset.py
@@ -52,54 +52,6 @@ class QCNetDataset(BaseDataset):
 
         return batch_data
 
-
-    # def collate_fn(self, data_list):
-    #     batch_data = HeteroData()
-
-    #     batch_data['scenario_id'] = [data[0]['scenario_id'] for data in data_list]
-    #     batch_data['map_center'] = torch.tensor(np.stack([data[0]['map_center'] for data in data_list]), dtype=torch.float32)
-
-    #     # agent
-    #     for data in data_list:
-    #         for key in data[0]['agent'].keys():
-    #             if key == 'num_nodes':
-    #                 if key not in batch_data['agent']:
-    #                     batch_data['agent'][key] = data[0]['agent'][key]
-    #                 else:
-    #                     batch_data['agent'][key] += data[0]['agent'][key]
-    #             else:
-    #                 batch_data['agent'][key] = torch.cat(
-    #                     [torch.tensor(data[0]['agent'][key]) for data in data_list], dim=0
-    #                 )
-
-    #     # map_point
-    #     for data in data_list:
-    #         for key in data[0]['map_point'].keys():
-    #             batch_data['map_point'][key] = torch.tensor(
-    #                 np.stack([data[0]['map_point'][key] for data in data_list])
-    #             )
-
-    #     # map_polygon
-    #     for data in data_list:
-    #         for key in data[0]['map_polygon'].keys():
-    #             batch_data['map_polygon'][key] = torch.tensor(
-    #                 np.stack([data[0]['map_polygon'][key] for data in data_list])
-    #             )
-
-    #     # map_polylines
-    #     for data in data_list:
-    #         for key in data[0]['map_polylines'].keys():
-    #             batch_data['map_polylines'][key] = torch.tensor(
-    #                 np.stack([data[0]['map_polylines'][key] for data in data_list])
-    #             )
-
-    #     # indexing
-    #     for data in data_list:
-    #         batch_data['map_point', 'to', 'map_polygon'].edge_index = torch.tensor(
-    #             np.stack([data[0]['map_point', 'to', 'map_polygon'].edge_index])
-    #         )
-
-    #     return batch_data
 class TargetBuilder(BaseTransform):
 
     def __init__(self,
@@ -209,17 +161,9 @@ def dict_to_heterodata(input_dict):
     hetero_data['map_polylines']['center'] = input_dict['map_polylines_center']
     
     # pt & pl indexing
-    num_polylines = map_polylines.shape[0]
-    num_points_per_polyline = map_polylines.shape[1] - 1
-    point_to_polygon_edge_index = np.stack([
-        np.arange(num_polylines * num_points_per_polyline, dtype=np.int64),
-        np.tile(np.arange(num_polylines, dtype=np.int64), num_points_per_polyline)
-    ], axis=0)
-    hetero_data['map_point', 'to', 'map_polygon']['edge_index'] = point_to_polygon_edge_index
-    
-    polygon_to_polygon_edge_index = []
-    for i in range(num_polylines - 1):
-        polygon_to_polygon_edge_index.append(np.array([[i, i+1], [i+1, i]]))
-    polygon_to_polygon_edge_index = np.concatenate(polygon_to_polygon_edge_index, axis=1) if polygon_to_polygon_edge_index else np.empty((2, 0), dtype=np.int64)
+    num_polylines, num_points_per_polyline = map_polylines.shape[0], map_polylines.shape[1] - 1
+    point_index = np.arange(num_polylines * num_points_per_polyline, dtype=np.int64)
+    polygon_index = np.repeat(np.arange(num_polylines, dtype=np.int64), num_points_per_polyline)
+    hetero_data['map_point', 'to', 'map_polygon']['edge_index'] = np.stack([point_index, polygon_index], axis=0)
     
     return [hetero_data]
